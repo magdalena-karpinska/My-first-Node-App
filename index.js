aa304144fc016b3345d05b72ccfab6e617a8a6a6
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-04-03 17:28:50 +0200

    Completes a checkup exercise

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const prompt = require('prompt-sync')();
 
 function greet(namePassedIn) {
-  return 'Welcome to SALT, ';
+  return 'Welcome to SALT, ' + namePassedIn;
+}
+
+function askDate(datePassedIn) {
+  return 'Today is 32 days left until the course starts'
 }
 
 // Main program
 const name = prompt('What is your name? ');
+const date = prompt('When does that course start? ');
+
 const greeting = greet(name);
 console.log(greeting);
+
+const schedule = askDate(date);
+console.log(schedule);
 
 module.exports.greet = greet;
